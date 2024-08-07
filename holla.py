bb28b7eeefde2dbe1cabf117e545807b4f75d7f9
Ismail <you@gmaile.com>
2024-08-07 21:16:22 +0500

    xea

---
+++ holla.py
@@ -0,0 +1,51 @@
+# class Car():
+#     def __init__(self, made, model, year):
+#         self.made = made
+#         self.model = model
+#         self.year = year 
+    
+#     def CarPark(self):
+#         return f"{self.made},{self.model}"
+    
+
+# # class Shape():
+#     def __init__(self):
+#         pass
+#     def area(self)
+    
+# class Task():
+#     def __init__(self, title, description, status) -> None:
+#         self.title = title 
+#         self.description = description
+#         self.status = status
+
+#     def scentence(self):
+#         return (f"TASK - {self.title} \nDESCRIPTION - {self.description} \nSTATUS - {self.status}")
+
+# c = input("Status \n")
+# b = input("Description\n")
+# c = input("Status \n")
+# result = Task(a,b,c)
+# print(result.scentence)
+
+
+# import math
+# a = 3
+# b = 4
+# gepotenuzakv = (math.pow(a,2) + math.pow(b,2))
+# gepotenuza = math.sqrt(gepotenuzakv)
+# print(gepotenuza)
+
+
+# import math
+
+# x = math.pi/4
+# f = math.sin(x) + math.cos(x)
+# print(f)
+
+# import math
+
+# r = 7 
+# C = r * 2 * math.pi
+# S = math.pow(r,2) * math.pi
+# print(f"периметр = {C}\nплощадь = {S}")
